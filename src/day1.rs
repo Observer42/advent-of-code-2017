ebaf39ec32351d2ce73349585b70244e26d6d686
Observer42 <yishengxu@outlook.com>
2018-09-30 02:49:48 +0800

    day 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,25 @@
-pub fn solve_captcha_first(input: &str) -> u32 {
+use std::fs::File;
+use std::io::Read;
+use std::io::Result;
+
+pub fn solve() -> Result<()> {
+    let mut file = File::open("src/input/01.txt")?;
+    let mut digits = String::new();
+    file.read_to_string(&mut digits)?;
+    let digits = digits.trim();
+    let day_1_first = solve_captcha_first(digits);
+    println!("day 1 first: {}", day_1_first);
+    let day_1_second = solve_captcha_second(digits);
+    println!("day 1 second: {}", day_1_second);
+    Ok(())
+}
+
+fn solve_captcha_first(input: &str) -> u32 {
     let func = |x| x + 1;
     solve_captcha(input, &func)
 }
 
-pub fn solve_captcha_second(input: &str) -> u32 {
+fn solve_captcha_second(input: &str) -> u32 {
     let half = input.len() / 2;
     let func = move |x| x + half;
     solve_captcha(input, &func)
@@ -23,11 +39,10 @@ fn solve_captcha(input: &str, func: &Fn(usize) -> usize) -> u32 {
 
 #[cfg(test)]
 mod tests {
-
     use super::{solve_captcha_first, solve_captcha_second};
 
     #[test]
-    fn first_problem_first_part() {
+    fn test_solve_captcha_first() {
         assert_eq!(solve_captcha_first("1122"), 3);
         assert_eq!(solve_captcha_first("1111"), 4);
         assert_eq!(solve_captcha_first("1234"), 0);
@@ -35,7 +50,7 @@ mod tests {
     }
 
     #[test]
-    fn first_problem_second_part() {
+    fn test_solve_captcha_second() {
         assert_eq!(solve_captcha_second("1212"), 6);
         assert_eq!(solve_captcha_second("1221"), 0);
         assert_eq!(solve_captcha_second("123425"), 4);
---
+++ src/day2.rs
@@ -0,0 +1,66 @@
+use std::fs::File;
+use std::io::Read;
+use std::io::Result;
+
+pub fn solve() -> Result<()> {
+    let mut file = File::open("src/input/02.txt")?;
+    let mut lines = String::new();
+    file.read_to_string(&mut lines)?;
+
+    let matrix = generate_matrix(&lines);
+    println!("day 2 first: {}", solve_checksum_first(&matrix));
+    println!("day 2 second: {}", solve_checksum_second(&matrix));
+    Ok(())
+}
+
+fn generate_matrix(input: &str) -> Vec<Vec<u32>> {
+    input.lines()
+        .map(|line| {
+            line.split_whitespace()
+                .filter_map(|digits| digits.parse::<u32>().ok())
+                .collect()
+        })
+        .collect()
+}
+
+fn solve_checksum_first(matrix: &[Vec<u32>]) -> u32 {
+    matrix.iter()
+        .map(|line| line.iter().max().unwrap() - line.iter().min().unwrap())
+        .sum()
+}
+
+fn solve_checksum_second(matrix: &[Vec<u32>]) -> u32 {
+    matrix.iter()
+        .filter_map(|line| {
+            for i in 0..(line.len() - 1) {
+                for j in (i + 1)..line.len() {
+                    if line[i] % line[j] == 0 {
+                        return Some(line[i] / line[j]);
+                    } else if line[j] % line[i] == 0 {
+                        return Some(line[j] / line[i]);
+                    }
+                }
+            }
+            None
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{solve_checksum_first, solve_checksum_second, generate_matrix};
+
+    #[test]
+    fn test_solve_checksum_first() {
+        let input = "5 1 9 5\n7 5 3\n2 4 6 8";
+        let matrix = generate_matrix(input);
+        assert_eq!(solve_checksum_first(&matrix), 18);
+    }
+
+    #[test]
+    fn test_solve_checksum_second() {
+        let input = "5 9 2 8\n9 4 7 3\n3 8 6 5";
+        let matrix = generate_matrix(input);
+        assert_eq!(solve_checksum_second(&matrix), 9);
+    }
+}
